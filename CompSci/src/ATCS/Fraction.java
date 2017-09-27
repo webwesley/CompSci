21db7646356f87425e3f6d58a81b817eba62808f
waford <waford@Gravity>
2017-09-27 13:20:51 -0400

    abother commit

--- CompSci/src/ATCS/Fraction.java
+++ CompSci/src/ATCS/Fraction.java
@@ -1,26 +1,57 @@
 package ATCS;
 
 public class Fraction {
-	int numTest;
-	int demTest; 
-	
-	public Fraction() {
-	
-		
-		}
-	
-	
-	public String above() {	
-		int numTest = 17;
-	
-		for(int demTest = 77; demTest <= 100; demTest++) {
-			
-		}
-		
-	}
-		
-	
-	
-	
+	double numTest;
+	double demTest;
+	double target;
 
+	public Fraction() {
+		this.numTest = 17;
+		this.demTest = 76;
+		this.target = (numTest / demTest);
+	}
+
+	public String above() {
+		
+		double difference = 1;
+		double numBest = 0;
+		double demBest = 0;
+		for (demTest = 1; demTest < 100; demTest++) {
+			for (numTest = 1; numTest < 100; numTest++) {
+				double tmp = (numTest / demTest) - target;
+				if (tmp < difference && (numTest / demTest - target) > 0) {
+					numBest = numTest;
+					demBest = demTest;
+					difference = tmp;
+					continue;
+				}
+			
+				}
+			}
+
+		return(numBest + "/"  + demBest);
+
+	}
+
+public String below() {
+		
+		double difference = 1;
+		double numBest = 0;
+		double demBest = 0;
+		for (demTest = 100; demTest > 0; demTest--) {
+			for (numTest = 100; numTest > 0; numTest--) {
+				double tmp = target - (numTest / demTest);
+				if (tmp < difference && tmp > 0) {
+					numBest = numTest;
+					demBest = demTest;
+					difference = tmp;
+					continue;
+				}
+			
+				}
+			}
+
+		return(numBest + "/"  + demBest);
+
+	}
 }
--- CompSci/src/ATCS/TesterACTS.java
+++ CompSci/src/ATCS/TesterACTS.java
@@ -4,7 +4,9 @@ public class TesterACTS {
 	
 	public static void main(String[] args) {
 		Fraction frac = new Fraction();
+
 		System.out.println(frac.above());
+		System.out.println(frac.below());
 		
 	}
 
