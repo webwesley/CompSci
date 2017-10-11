7d70528320cfda0e0f5800c2066ab33aa8485e5c
waford <waford@Gravity>
2017-10-11 14:30:02 -0400

    Another Commit

--- CompSci/src/guis/ChoseYourOwnAdventure.java
+++ CompSci/src/guis/ChoseYourOwnAdventure.java
@@ -15,87 +15,107 @@ public class ChoseYourOwnAdventure {
 	String text;
 	ChoseYourOwnAdventure choice1;
 	ChoseYourOwnAdventure choice2;
-	
+	JFrame frame;
+
 	public ChoseYourOwnAdventure(ChoseYourOwnAdventure tmp) {
-		this(tmp.text, tmp.name, tmp.choice1, tmp.choice2);
-	}
-	
-	public ChoseYourOwnAdventure(String text, String name) {
-		this.name = name;
-		JFrame frame = new JFrame("James Howard's Space Adventure");
-		frame.setVisible(true);
+		this.name = tmp.name;
+		this.text = tmp.text;
+		if(tmp.choice1 != null && tmp.choice2 != null) {
+			this.choice1 = tmp.choice1;			
+			this.choice2 = tmp.choice2;
+		} 
+		this.frame = new JFrame("James Howard's Space Adventure");
+		frame.setVisible(false);
 		frame.setSize(400, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		//creates the pannelclass Action1 extends ActionListener()
-		//attaches pannel to frame
 		JPanel panel = new JPanel();
 		frame.add(panel);
+		panel.setLayout(new GridLayout(3, 2));
+
+		JLabel setting = new JLabel(this.text);
+		panel.add(setting);
+
+		JButton pick1 = new JButton(this.choice1.name);
+		JButton pick2 = new JButton(this.choice2.name);
+
+		pick1.addActionListener(new Action1());
+		pick2.addActionListener(new Action2());
+		
+		panel.add(pick1);
+		panel.add(pick2);
+	}
+
+	public ChoseYourOwnAdventure(String text, String name) {
+		this.name = name;
+		this.frame = new JFrame("James Howard's Space Adventure");
+		frame.setVisible(false);
+		frame.setSize(400, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// creates the pannelclass Action1 extends ActionListener()
+		// attaches pannel to frame
+		JPanel panel = new JPanel();
+		frame.add(panel);
+
 		panel.setLayout(new FlowLayout());
 		JLabel setting = new JLabel(text);
 		panel.add(setting);
 	}
-	
-	public ChoseYourOwnAdventure(String text, String name, ChoseYourOwnAdventure choice1, ChoseYourOwnAdventure choice2) {
+
+	public ChoseYourOwnAdventure(String text, String name, ChoseYourOwnAdventure choice1,
+			ChoseYourOwnAdventure choice2) {
 		this.name = name;
 		this.text = text;
 		this.choice1 = choice1;
 		this.choice2 = choice2;
-		JFrame frame = new JFrame("James Howard's Space Adventure");
-		frame.setVisible(true);
+		this.frame = new JFrame("James Howard's Space Adventure");
+		frame.setVisible(false);
 		frame.setSize(400, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//creates the pannel
-		//attaches pannel to frame
+
+		// creates the pannel
+		// attaches pannel to frame
 		JPanel panel = new JPanel();
 		frame.add(panel);
-		panel.setLayout(new GridLayout(2,2));
-		
+		panel.setLayout(new GridLayout(3, 2));
+
 		JLabel setting = new JLabel(this.text);
 		panel.add(setting);
-		
-		JButton pick1 = new JButton(choice1.name);
-		JButton pick2 = new JButton(choice2.name);
-		
+
+		JButton pick1 = new JButton(this.choice1.name);
+		JButton pick2 = new JButton(this.choice2.name);
+
 		pick1.addActionListener(new Action1());
 		pick2.addActionListener(new Action2());
+		
+		panel.add(pick1);
+		panel.add(pick2);
 	}
-	
-}
 
-class Action1 implements ActionListener {
-
-	public void actionPerformed(ActionEvent e, ChoseYourOwnAdventure choice1) {
-		// TODO Auto-generated method stub
-		
-		
-		
+	public void draw() {
+		frame.setVisible(true);
 	}
-	
+
+	class Action1 implements ActionListener {
+
+
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			ChoseYourOwnAdventure tmp = new ChoseYourOwnAdventure(ChoseYourOwnAdventure.this.choice1);
+			tmp.draw();
+		}
+
+	}
+
+	class Action2 implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			ChoseYourOwnAdventure tmp = new ChoseYourOwnAdventure(ChoseYourOwnAdventure.this.choice2);
+			tmp.draw();
+
+		}
+
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- CompSci/src/guis/TesterClass.java
+++ CompSci/src/guis/TesterClass.java
@@ -1,12 +1,13 @@
 package guis;
 
-import java.awt.Graphics;
 
 public class TesterClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		ChoseYourOwnAdventure first = new ChoseYourOwnAdventure("This is a test", "This is a title");
+		ChoseYourOwnAdventure second = new ChoseYourOwnAdventure("This is another test", "This is another title");
+		ChoseYourOwnAdventure test = new ChoseYourOwnAdventure("This is the ultimate test", "This is the test title", first, second);
+		test.draw();
 	}
 
 }
