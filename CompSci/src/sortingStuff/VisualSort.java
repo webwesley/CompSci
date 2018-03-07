d328b03e60cd6b3e55cd8956244f92f3d971f034
waford <waford@nebula>
2018-03-07 14:57:27 -0500

    Final Visual Sort

--- CompSci/src/sortingStuff/VisualSort.java
+++ CompSci/src/sortingStuff/VisualSort.java
@@ -2,25 +2,26 @@ package sortingStuff;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
+import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class VisualSort extends JPanel implements ActionListener {
+public class VisualSort extends JPanel {
 
 	Integer[] array;
-	Timer time;
 	int resizeFactor; //just something so i can keep testing small numbers but get larger boxes
+	int pointer; //just a variable to make the pointed at box highlighted
 	
 	public VisualSort(Integer[] array) {
 		this.array = array;
-		resizeFactor = 15;
-		time = new Timer(1000, this);
-		time.start();
+		resizeFactor = 9;
+		pointer = 0;
 	}
 	
 	public void paintComponent(Graphics g) {
@@ -31,59 +32,99 @@ public class VisualSort extends JPanel implements ActionListener {
 	
 	
 	private void paintRectangles(Integer[] array, Graphics g) {
-		System.out.println(Arrays.toString(array));
+		//System.out.println(Arrays.toString(array));
 		int width = this.getWidth() / array.length;
 		int x = 0;
 		int height = this.getHeight();
 		for(int i = 0; i < array.length; i++) {
-			if(i % 2 == 0) {
-				g.setColor(Color.GREEN);
-			} else {
+			if(i == pointer) {
 				g.setColor(Color.RED);
+				int bottomY = height;
+				int topY = height - this.resizeFactor * array[i];
+				g.fillRect(x, topY, width, bottomY);
+			} else {
+				g.setColor(Color.BLACK);
+				int bottomY = height;
+				int topY = height - this.resizeFactor * array[i];
+				g.drawRect(x, topY, width, bottomY);
+				g.setColor(Color.BLUE);
+				g.fillRect(x, topY, width, bottomY);
 			}
-			int bottomY = height;
-			int topY = height - this.resizeFactor * array[i];
-			g.fillRect(x, topY, width, bottomY);
-			System.out.println("Printed Rect at " + x + "," + " Width: " + width + "Height: " + height );
+			
+			//System.out.println("Printed Rect at " + x + "," + " Width: " + width + "Height: " + height );
 			x += width;
 		}
 		
 	}
 
-	
 
-
-	public void actionPerformed(ActionEvent e) {
+	public void bubbleSort() {
 			boolean sorted = true;
-			for(int i = array.length - 2; i>1; i--) {
+			for(int i = array.length - 1; i >= 1; i--) {
 				sorted = true;
-				for(int j = 0; j <i-1; j++) {
-					if(array[i].compareTo(array[j+1]) > 0) {
+				for(int j = 0; j < i; j++) {
+					pointer = j;
+					try {
+						Thread.sleep(0);
+					} catch (InterruptedException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					if(array[j].compareTo(array[j+1]) > 0) {
 						sorted = false;
 						int tmp = array[j];
 						array[j] = array[j+1];
 						array[j+1] = tmp;
-						this.repaint();
-						return;
+						
 					}
+					paint(getGraphics());
+					Toolkit.getDefaultToolkit().sync(); //this is something to make the animation on linux slightly smoother
 				}
 				if(sorted) {
-					time.stop();
+				
+					System.out.println("Done");
+					return;
 				}
 			}
-
-		
+	}
+	
+	public void quickSort(int low, int high) {
+		if(low == high) {
+			return;
+		} else {
+			Integer pivot = array[0];
+			int pivotIndex = 0;
+			for(int i = low; i < high; i++ ) {
+				if(pivot.compareTo(array[i]) < 0) {
+					int tmp = array[i];
+					array[i] = pivot;
+					array[pivotIndex] = tmp;
+					pivotIndex = i;
+				}
+			}
+			quickSort()
+		}
 	}
 	
 	public static void main(String[] args) {
-		Integer[] small = {1, 5, 2, 3};
-		VisualSort test = new VisualSort(small);
+		Integer[] testArray = makeTestArray(200, 100);
+		VisualSort test = new VisualSort(testArray);
 		JFrame frame = new JFrame();
 		frame.setTitle("Test Stuff");
-		frame.setSize(400, 400);
+		frame.setSize(1000, 1000);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(test);
 		frame.setVisible(true);
+		test.bubbleSort();
+	}
+
+	private static Integer[] makeTestArray(int iterations, int range) {
+		Integer[] tmp = new Integer[iterations];
+		Random random = new Random();
+		for(int i = 0; i < iterations; i++) {
+			tmp[i] = random.nextInt(range - 1) + 1;
+		}
+		return tmp;
 	}
 
 }
