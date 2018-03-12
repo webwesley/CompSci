0886104dd989d875aae8f9b6ab932772a81b540c
waford <waford@nebula>
2018-03-11 23:12:25 -0400

    Workign RAdix SOrt

--- CompSci/src/sortingStuff/radixSort.java
+++ CompSci/src/sortingStuff/radixSort.java
@@ -1,6 +1,7 @@
 package sortingStuff;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class radixSort {
 
@@ -9,7 +10,9 @@ public class radixSort {
 
 	public radixSort(int[] array) {
 		numbers = new ArrayList<ArrayList<Integer>>(10);
-		for(int i )
+		for(int i = 0; i < 10; i++) {
+			numbers.add(new ArrayList<Integer>());
+		}
 		this.array = array;
 	}
 
@@ -17,7 +20,7 @@ public class radixSort {
 		int iterations = getIterations();
 		for (int i = 0; i < iterations; i++) {
 			for (int j = 0; j < array.length; j++) {
-				int tmp = (int) (array[j] % Math.pow(10, i));
+				int tmp = (int) (array[j] % Math.pow(10, i + 1) / Math.pow(10,  i));
 				numbers.get(tmp).add(array[j]);
 			}
 			resetArray();
@@ -31,7 +34,12 @@ public class radixSort {
 		for(int i = 0; i < numbers.size(); i++) {
 			for(int j = 0; j < numbers.get(i).size(); j++) {
 				array[index] = numbers.get(i).get(j);
+				index++;
 			}
+		}
+		numbers = new ArrayList<ArrayList<Integer>>(10); //resets the numbers double arraylist
+		for(int i = 0; i < 10; i++) {
+			numbers.add(new ArrayList<Integer>());
 		}
 		
 	}
@@ -44,7 +52,7 @@ public class radixSort {
 	private int findMaximum() {
 		int max = array[0];
 		for (int i = 1; i < array.length; i++) {
-			if (max > array[i]) {
+			if (max < array[i]) {
 				max = array[i];
 			}
 		}
@@ -52,8 +60,9 @@ public class radixSort {
 	}
 	
 	public static void main(String[] args) {
-		int[] tmp = {21, 31, 105, 101};
+		int[] tmp = {21, 31, 200000, 105, 101, 5, 11, 209};
+		System.out.println("Array Before: " + Arrays.toString(tmp));
 		radixSort test = new radixSort(tmp);
-		test.sort();
+		System.out.println("Array after sorting: " + Arrays.toString(test.sort()));
 	}
 }
